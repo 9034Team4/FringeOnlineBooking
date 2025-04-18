ed22ff2e37b5d38379ee320549d0f275eb212e00
MasonWen028 <funrie028@gmail.com>
2025-04-18 22:58:32 +0930

    CI/CD

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,10 +5,10 @@ import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST && '127.0.0.1',
   port: 3306,
   username: process.env.DB_USER && 'root',
-  password: process.env.DB_PASSWORD && '',
+  password: process.env.DB_PASSWORD && 'root123',
   database: process.env.DB_NAME && 'fringe2025bookingdb_dev',
   synchronize: false,
   logging: false,
