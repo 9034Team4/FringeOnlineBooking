9ac1da182c6c921f153c2e86fbd58def6ff5a8ee
MasonWen028 <funrie028@gmail.com>
2025-04-18 22:46:34 +0930

    test redis and mysql working or not

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,9 +7,9 @@ export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: process.env.DB_USER && 'root',
+  password: process.env.DB_PASSWORD && '',
+  database: process.env.DB_NAME && 'fringe2025bookingdb_dev',
   synchronize: false,         // 🔁 true in dev, false in prod (use migrations)
   logging: false,
   entities: [__dirname + '/../entities/*.ts'],
