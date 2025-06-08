bced31fb3112cd26547a88d0c8ac90b3d74d42bd
MasonWen028 <funrie028@gmail.com>
2025-06-08 17:42:16 +0930

    update: fix the docker-copmpose file, fix the errors in the data-srouces.ts,  #deploy-backend

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '3306'),
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '12345678',
+    password: process.env.DB_PASSWORD || 'root123',
     database: process.env.DB_DATABASE || 'fringe2025bookingdb_dev',
     synchronize: true,
     logging: false,
