b880271baaa1aa23304fd3eef488cc3a2abd3453
MasonWen028 <funrie028@gmail.com>
2025-06-09 21:04:16 +0930

    #deploy-backend

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -34,9 +34,9 @@ export const AppDataSource = new DataSource({
     //     EventCategory, Venue, Seat, Message
     // ],
     // 如果上面的方法在生产环境中不起作用，可以尝试使用以下配置
-    entities: isProduction 
-      ? [path.join(__dirname, '..', '**', '*.entity.{js,ts}'), path.join(__dirname, '..', 'entities', '**', '*.js')]
-      : [path.join(__dirname, '..', 'entities', '**', '*.ts')],
+    entities: isProduction
+    ? [path.join(__dirname, 'entities', '*.js')]
+    : [path.join(__dirname, '..', 'entities', '*.ts')],
     migrations: isProduction
       ? [path.join(__dirname, '..', 'migrations', '**', '*.js')]
       : [path.join(__dirname, '..', 'migrations', '**', '*.ts')],
